bdc887df84704ceec15ec91be3d74574052a2ba0
Nishibayashi Takuji <takuji@senchan.jp>
2011-09-23 15:06:47 +0900

    基本的なルーティング

--- lib/TwitterPaste/Web/Router.pm
+++ lib/TwitterPaste/Web/Router.pm
@@ -4,5 +4,31 @@ use warnings;
 
 use Chiffon::Web::Router;
 
+connect(
+    '/login',
+    {controller => 'Root', action => 'login'},
+    {},
+);
+
+connect(
+    '/callback',
+    {controller => 'Root', action => 'login'},
+    {},
+);
+
+connect(
+    '/add',
+    {controller => 'Root', action => 'login'},
+    {},
+);
+
+connect(
+    '/:uuid',
+    {controller => 'Root', action => 'show'},
+    {
+        pass => [qw(uuid)]
+    },
+);
+
 1;
 
